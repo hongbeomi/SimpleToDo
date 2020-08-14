c6c5a0cd50fbf2149536659eb59dc70701fe58c2
hongbeomi <dks3963@naver.com>
2020-08-14 17:35:06 +0900

    add result

--- lib/presentation/task_view_model.dart
+++ lib/presentation/task_view_model.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:simpletodo/data/model/task.dart';
 import 'package:simpletodo/data/repository/task_local_repository.dart';
 
-class TaskViewModel with ChangeNotifier {
+class TaskViewModel extends ChangeNotifier {
   final TaskLocalRepository repository = TaskLocalRepository();
 
   Future<List<Task>> loadTasks() async {
